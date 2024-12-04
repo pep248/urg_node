518f9b86b5b0a9a4f8cb511444ad785ed8998681
Josep Rueda <rueda_999@hotmail.com>
2024-12-04 08:34:51 +0100

    added launch file

--- launch/launch_lidar.launch.py
+++ launch/launch_lidar.launch.py
@@ -4,48 +4,54 @@ from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
 import os
+from xacro import process_file
 
 def generate_launch_description():
     # Paths
+    package_name = 'urg_node'
+    
     urg_node_launch_file = PathJoinSubstitution([
         FindPackageShare("urg_node"),
         "launch",
         "urg_node_launch.py"
     ])
     
-    urdf_file = PathJoinSubstitution([
-        FindPackageShare("urg_node"),
-        "urdf",
-        "hokuyo_laser.urdf"
-    ])
+    xacro_file_path = os.path.join(
+        get_package_share_directory(package_name),
+        'urdf',
+        'hokuyo_laser.urdf.xacro'
+    )
+
+    # Process the xacro file to generate URDF content
+    urdf_file = process_file(xacro_file_path).toxml()
     
     rviz_config = PathJoinSubstitution([
         FindPackageShare("urg_node"),
         "rviz",
-        "rviz.rviz"
+        "lidar.rviz"
     ])
 
     # Launch Description
     return LaunchDescription([
         # Static Transform Publisher
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            arguments=['--x', '0', '--y', '0', '--z', '0',
-                      '--roll', '0', '--pitch', '0', '--yaw', '0',
-                      '--frame-id', 'world',
-                      '--child-frame-id', 'laser'],
-            output='screen'
-        ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     arguments=['--x', '0', '--y', '0', '--z', '0',
+        #               '--roll', '0', '--pitch', '0', '--yaw', '0',
+        #               '--frame-id', 'world',
+        #               '--child-frame-id', 'laser'],
+        #     output='screen'
+        # ),
         
         # Robot State Publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            name='robot_state_publisher',
             output='screen',
-            arguments=[urdf_file]
+            parameters=[{'robot_description': urdf_file}],
         ),
         
         # Include URG Node Launch
